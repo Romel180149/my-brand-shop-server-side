8054fd1befa55dca933e256565a12f5cbff00d07
Romel180149 <mosiurrahmanromel180149@gmail.com>
2024-05-12 23:40:30 +0600

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port= process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mosiurrahmanromel180149:27zUhUPu0VOuZipw@cluster0.jscktri.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jscktri.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
